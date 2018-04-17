2a3fd3f9aceaab455fc164255368cd5b229c7899
Kristin Bradley <me@kristinbradley.com>
2018-04-16 21:44:30 -0700

    Code cleanup, Functionaliity for photo nav controls, improved layout for modal photo viewer

--- js/app.js
+++ js/app.js
@@ -2,67 +2,131 @@
 // * Use ES6
 // * Make JS code as simple and clear as possible
 
-
 const photosUrl = 'https://api.flickr.com/services/rest?method=flickr.photos.search&format=json&text=dogs&api_key=e422fa8254715345e057d34a408f2a7d&safe_search=1&extras=url_s,url_m&nojsoncallback=1';
 
 // Get photo data:
 fetch(photosUrl)
 .then(response => response.json())
-.then(photos => displayPhotoGrid(photos))
-.catch(error => console.log('There was an error: ' + error));
+.then(data => displayPhotoGrid(data))
+.catch(error => console.log('There was an error: ' + error))
+;
 
 // Display photo grid:
-function displayPhotoGrid(photos) {
+function displayPhotoGrid(data) {
     const photosUl = document.getElementById('photo-grid');
     let listItem = '';
+    let photoIndex = 0;
 
-    photos.photos.photo.forEach(function (photo) {
+    data.photos.photo.forEach(function (photo) {
+        photoObject.photos.push(photo);
+
         listItem += `
       <li>
-        <a href="${photo.url_m}" data-title="${photo.title}" onclick="displayModal(this); return false;">
+        <a href="${photo.url_m}" data-index="${photoIndex}" onclick="modal.display(this); return false;">
           <img src="${photo.url_s}" alt="${photo.title}">
         </a>
       </li>
     `;
+
+        photoIndex++;
     });
 
     photosUl.innerHTML += listItem;
 }
 
+// Modal Object ------------------
+const modal = {
+    // Properties:
+    modal: document.getElementById('modal-container'),
+    modalTitle: document.getElementById('modal-title'),
+    modalPhoto: document.getElementById('modal-photo'),
+    modalButton: document.getElementById('modal-button'),
 
-// Modal Functions
-function showModal(modal) {
-    modal.classList.add("is-open");
-}
+    // Methods:
+    show: function () {
+        this.modal.classList.add("is-open");
+    },
 
-function hideModal(modal) {
-    modal.classList.remove("is-open");
-}
+    hide: function () {
+        this.modal.classList.remove("is-open");
+    },
 
-function displayModal(elem) {
-    // Get Photo Url:
-    const thisPhotoUrl = elem.getAttribute('href');
+    display: function (elem) {
+        // Get Index:
+        const elemIndex = elem.getAttribute('data-index');
+        const photo = photoObject.getPhotoByIndex(elemIndex);
 
-    // Modal Title:
-    const thisTitle = elem.getAttribute('data-title');
-    const modalTitle = document.getElementById('modal-title');
-    modalTitle.innerHTML = thisTitle;
+        // Get Photo Url & Title:
+        const thisPhotoUrl = photo.url_m;
+        const thisTitle = photo.title;
 
-    // Modal Body Photo
-    const modalBodyPhoto = document.getElementById('modal-photo');
+        // Insert Modal Title:
+        this.modalTitle.innerHTML = thisTitle;
 
-    // photo
-    modalBodyPhoto.innerHTML = `
-    <img src="${thisPhotoUrl}" alt="">
-  `;
+        // Insert Modal Body Photo:
+        this.modalPhoto.innerHTML = `
+      <img src="${thisPhotoUrl}" alt="">
+    `;
 
-    // Modal:
-    const modal = document.getElementById('modal');
-    showModal(modal);
+        // Display Modal:
+        this.show();
+    },
 
-    // Button:
-    const modalButton = document.getElementById('modal-button');
-    modalButton.onclick = function() {
-        hideModal(modal);
-    };
-}
+    previousPhoto: function () {
+        let photo = photoObject.getPrevious();
+
+        // Insert Modal Title:
+        this.modalTitle.innerHTML = photo.title;
+
+        // Insert Modal Body Photo:
+        this.modalPhoto.innerHTML = `
+      <img src="${photo.url_m}" alt="">
+    `;
+    },
+
+    nextPhoto: function () {
+        let photo = photoObject.getNext();
+
+        // Insert Modal Title:
+        this.modalTitle.innerHTML = photo.title;
+
+        // Insert Modal Body Photo:
+        // (note: alt value is empty since it would be redundant with title)
+        this.modalPhoto.innerHTML = `
+      <img src="${photo.url_m}" alt="">
+    `;
+    }
+};
+
+// Photo Navigation -----------------------
+const photoObject = {
+    // Properties:
+    photos: [],
+    index: 0,
+
+    // Methods:
+    getPhotoByIndex: function (index) {
+        this.index = index;
+        return this.photos[index];
+    },
+
+    getPrevious: function () {
+        this.index--;
+
+        if (this.index < 0) {
+            this.index = this.photos.length - 1;
+        }
+
+        return this.getPhotoByIndex(this.index);
+    },
+
+    getNext: function () {
+        this.index++;
+
+        if (this.index > this.photos.length - 1) {
+            this.index = 0;
+        }
+
+        return this.getPhotoByIndex(this.index);
+    }
+};
